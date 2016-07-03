93df99fdaaf1ac882317539ce8fef33244b95f01
Benji <benjipollock@gmail.com>
2016-07-03 21:02:15 +0300

    Added funtionalities and option, still very buggy.

--- MemoryGame.js
+++ MemoryGame.js
@@ -2,12 +2,57 @@
  * Created by itc_user on 6/30/2016.
  */
 document.body.style.backgroundImage = "url(images/bg.jpg)";
+
 var cardsToFlip = [];
 var clickDisabled = false;
+var clickedCards = [];
 var wrongnessCounter = 0;
-var galaxyBackground = "url('http://www.lostvalleyobservatory.com/sitebuildercontent/sitebuilderpictures/pelican.best.web.lrprgb.overlay.jpg')";
-var waterBackground = "url('http://www.freeiconspng.com/uploads/water-png-splash-of-water-2-10.png')"; 
-var coolBackground = "url(images/texture.jpg)";
+var backgrounds = {
+    "galaxies":"url('http://www.lostvalleyobservatory.com/sitebuildercontent/sitebuilderpictures/pelican.best.web.lrprgb.overlay.jpg')",
+    "water":"url('http://www.freeiconspng.com/uploads/water-png-splash-of-water-2-10.png')",
+    "tablecloth":"url(images/texture.jpg)"
+}
+var cardImages = {"animals":[
+    {src: "images/animals/1.jpg"},
+    {src: "images/animals/2.jpg"},
+    {src: "images/animals/3.jpg"},
+    {src: "images/animals/4.jpg"},
+    {src: "images/animals/5.jpg"},
+    {src: "images/animals/6.jpg"},
+    {src: "images/animals/7.jpg"},
+    {src: "images/animals/8.jpg"},
+    {src: "images/animals/9.jpg"},
+    {src: "images/animals/10.jpg"},
+    {src: "images/animals/11.jpg"},
+    {src: "images/animals/12.jpg"}
+], "logos":[
+    {src: "images/logos/facebook.png"},
+    {src: "images/logos/google.png"},
+    {src: "images/logos/instagram.png"},
+    {src: "images/logos/lamborghini.png"},
+    {src: "images/logos/linkedin.png"},
+    {src: "images/logos/pinterest.png"},
+    {src: "images/logos/reddit.png"},
+    {src: "images/logos/snapchat.png"},
+    {src: "images/logos/tesla.png"},
+    {src: "images/logos/tumblr.png"},
+    {src: "images/logos/twitter.png"},
+    {src: "images/logos/vine.png"}
+], "languages": [
+    {src: "images/coding/angular.png"},
+    {src: "images/coding/bootstrap.png"},
+    {src: "images/coding/c++.png"},
+    {src: "images/coding/django.png"},
+    {src: "images/coding/html.png"},
+    {src: "images/coding/java.png"},
+    {src: "images/coding/jquery.png"},
+    {src: "images/coding/js.png"},
+    {src: "images/coding/mongo.png"},
+    {src: "images/coding/node.png"},
+    {src: "images/coding/python.png"},
+    {src: "images/coding/ruby.png"}
+]};
+
 var animals = [
     {src: "images/animals/1.jpg"},
     {src: "images/animals/2.jpg"},
@@ -36,7 +81,7 @@ var logos = [
     {src: "images/logos/twitter.png"},
     {src: "images/logos/vine.png"}
 ];
-var flags = [
+var languages = [
     {src: "images/coding/angular.png"},
     {src: "images/coding/bootstrap.png"},
     {src: "images/coding/c++.png"},
@@ -51,23 +96,39 @@ var flags = [
     {src: "images/coding/ruby.png"}
 ];
 
-var pictures = animals;
-var background = coolBackground;
+var pictures = cardImages["logos"];
+var background = backgrounds["tablecloth"];
 var numDifCards = 8;
-//
-// var pictures = //choice
-// var background = //choice
-// var numDifCards = //choice
 
 
-var cardRandomizer = function (){
+var handleOptionChanged = function(e){
+    numDifCards = parseInt(document.getElementById("number-of-cards").value);
+    console.log("here")
+    if (e.target.className.indexOf("backgrounds-selector") != -1){
+        background = backgrounds[e.target.id];
+    }
+
+    if (e.target.className.indexOf("pictures-selector") != -1){
+        pictures = e.target.id;
+    }
+
+    createBoard();
+    return false;
+}
+
+var cardRandomizer = function (numDifCards, cardsToFlip){
+
+    var menuOptions = document.getElementsByClassName("menu-option");
+    for(var op = 0; op < menuOptions.length ; op++){
+        menuOptions[op].addEventListener("click",handleOptionChanged);
+    }
 
         for (var h = 0; h<numDifCards*2; h++){
                 cardsToFlip.push(pictures[h%numDifCards]);
+
         }
 
 
-    console.log(JSON.stringify(cardsToFlip));
 
     for (var i = cardsToFlip.length -1; i >= 0; i--){
         var j = Math.floor(Math.random()*(i+1));
@@ -75,33 +136,33 @@ var cardRandomizer = function (){
         cardsToFlip[i] = cardsToFlip[j];
         cardsToFlip[j] = temp;
     }
-    console.log(JSON.stringify(cardsToFlip));
+    return cardsToFlip;
 };
-window.onload = cardRandomizer();
+
 
 var displayCard = function(clickEvent){
     if (clickDisabled === false) {
         clickEvent.target.className = 'uncovered';
         clickEvent.target.style.backgroundImage = "url(" + clickEvent.target.getAttribute('src') + ")";
         clickedCards.push(clickEvent.target);
-        console.log(clickedCards);
-        console.log(clickedCards[0].getAttribute('src'));
 
         if (clickedCards.length === 2) {
             if (clickedCards[0].getAttribute('src') === clickedCards[1].getAttribute('src')) {
                 console.log("in loop" + clickedCards);
                 console.log('woohoo!');
+                clickedCards = [];
             }
             else {
                 clickDisabled = true;
                 setTimeout(function () {
                     clickedCards[0].className = 'covered';
-                    clickedCards[0].style.backgroundImage = "url('http://www.lostvalleyobservatory.com/sitebuildercontent/sitebuilderpictures/pelican.best.web.lrprgb.overlay.jpg')";
+                    clickedCards[0].style.backgroundImage = background;
                     clickedCards[1].className = 'covered';
-                    clickedCards[1].style.backgroundImage = "url(http://www.lostvalleyobservatory.com/sitebuildercontent/sitebuilderpictures/pelican.best.web.lrprgb.overlay.jpg)";
+                    clickedCards[1].style.backgroundImage = background;
                     clickedCards = [];
                     clickDisabled = false;
                     wrongnessCounter++;
+                    console.log(clickedCards);
                 }, 1000);
 
             }
@@ -110,10 +171,12 @@ var displayCard = function(clickEvent){
 };
 
 var createBoard = function(){
-    
+    cardRandomizer(numDifCards, cardsToFlip);
+    document.getElementsByClassName('gameboard')[0].innerHTML = "";
     for (var i = 0; i < numDifCards*2; i++){
         var card = document.createElement('div');
         card.className = 'covered';
+        card.style.backgroundImage = background;
         card.setAttribute('src', cardsToFlip[i].src);
         document.getElementsByClassName('gameboard')[0].appendChild(card);
         card.addEventListener('click', displayCard);
